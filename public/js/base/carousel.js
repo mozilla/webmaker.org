b47da5b74f2331b61377ce4217ab8a61902542e8
Kate Hudson <katehudsondesign@gmail.com>
2013-05-28 21:18:52 -0400

    [#877004] Requirejs config fix

--- public/js/base/carousel.js
+++ public/js/base/carousel.js
@@ -1,4 +1,4 @@
-define(['jquery'], function($) {
+define(['jquery', 'jquery.carousel'], function($) {
   'use strict';
   // variables & setup
   var $arrows = $('.cta-arrow'),
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
-define(['jquery', 'jquery-carousel', 'base/carousel', 'base/webmaker', 'base/mediaGallery', 'base/privacy', 'base/ui', 'uri' ],
-  function ( $, $carousel, carousel, webmaker, MediaGallery, privacy, UI, URI ) {
+requirejs.config({
+  paths: {
+    'jquery': '../ext/js/jquery-1.9.1',
+    'jquery.carousel': '../ext/js/jquery.carouFredSel-6.2.1',
+    'moment': '../ext/js/moment',
+    'tabzilla': 'https://www.mozilla.org/tabzilla/media/js/tabzilla',
+    'uri': '../ext/js/uri'
+  },
+  shim: {
+    'tabzilla': ['jquery'],
+    'jquery.carousel': ['jquery']
+  }
+});
+
+require(['jquery','base/carousel', 'base/webmaker', 'base/mediaGallery', 'base/privacy', 'base/ui', 'uri', 'tabzilla' ],
+  function ( $, carousel, webmaker, MediaGallery, privacy, UI, URI ) {
   'use strict';
   $(document).ready(function() {
     var $body = $('body'),
@@ -46,19 +60,6 @@ define(['jquery', 'jquery-carousel', 'base/carousel', 'base/webmaker', 'base/med
     UI.select( '#search-filter', function( val ) {
       switch ( val ) {
         case 'recent':
-          break;
-
-        // case 'recommended':
-        //   break;
-
-        case 'guide:popcorn':
-          media.search( { contentType: 'application/x-popcorn' } );
-          break;
-
-        case 'guide:thimble':
-          media.search( { contentType: 'application/x-thimble' } );
-          break;
-
         case 'title':
           media.search( { title: true } );
           break;
@@ -74,4 +75,4 @@ define(['jquery', 'jquery-carousel', 'base/carousel', 'base/webmaker', 'base/med
 
     privacy.attach();
   });
-  });
+});

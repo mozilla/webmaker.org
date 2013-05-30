f109244c71c541afa10a61067810a4310987e914
Kate Hudson <katehudsondesign@gmail.com>
2013-05-30 11:00:14 -0400

    [template/select fixes]

--- public/js/base/ui.js
+++ public/js/base/ui.js
@@ -1,40 +1,56 @@
-define( ["jquery"], function( $ ) {
+define( ["jquery", "text!html/ui-fragments.html" ], function( $, _fragments ) {
   "use strict";
 
-  var UI = {};
+  var UI = {},
+      $fragments = $( document.createElement( "div" ) ).html( _fragments );
 
-  UI.select = function( el, fn ) {
-    var $el = $( el ),
+  UI.select = function( select, fn ) {
+    var $el = $( ".ui-select", $fragments ).clone( true ),
         $toggleBtn = $el.find( ".icon" ),
         $selectedEl = $el.find( ".ui-selected" ),
-        $menu = $el.find( ".ui-select-menu" ),
-        $firstSelected = $el.find( "[data-selected]" );
+        $menuContainer = $el.find( ".ui-select-menu" ),
+        $menu = $menuContainer.find( "ul" ),
+        $li = $menu.find( "li" );
+
+    var $select = $( select ),
+        $options = $( "option", select );
 
     fn = fn || function() {};
 
-    $toggleBtn.click( function( e ) {
-      $menu.toggle();
+    $options.each( function( i, option ) {
+      var val = $( option ).val(),
+          html = $( option ).html(),
+          $newLi = $li.clone();
+      $newLi.attr( "data-value", val );
+      $newLi.html( html );
+      if ( $( option ).attr( "selected" ) ) {
+        $newLi.attr( "data-selected", true);
+        $selectedEl.html( html );
+      }
+      $newLi.click( function() {
+        var $this = $( this );
+
+        $menu.find( "[data-selected]" ).removeAttr( "data-selected" );
+        $( this ).attr( "data-selected", true );
+        $selectedEl.text( val );
+        $menuContainer.hide();
+        fn( val );
+        $select.val( val );
+
+      });
+      $menu.append( $newLi );
     });
 
     $selectedEl.click( function( e ) {
-      $menu.toggle();
+      $menuContainer.toggle();
+    });
+    $toggleBtn.click( function( e ) {
+      $menuContainer.toggle();
     });
 
-    $menu.click( function( e ) {
-      var $li = $( this ).find( "li" ),
-          $target = $( e.target );
-      $li.removeClass( "ui-on" );
-      $target.addClass( "ui-on" );
-      $selectedEl.html( $target.html() );
-      $menu.hide();
-      fn(  $target.data( "value" ) ) ;
-    });
-
-    $menu.hide();
-    if ( $firstSelected ) {
-      $selectedEl.html( $firstSelected.html() );
-      $firstSelected.addClass( "ui-on" );
-    }
+    $li.remove();
+    $el.insertAfter( $select );
+    $select.hide();
   };
 
   return UI;
--- public/js/base/webmaker.js
+++ public/js/base/webmaker.js
@@ -1,10 +1,11 @@
-define(['jquery', 'uri'],
-  function ($, URI ) {
+define(['jquery', 'uri', 'base/ui'],
+  function ($, URI, UI ) {
   'use strict';
 
   var makeURL,
       page,
-      make;
+      make,
+      setup = {};
 
   function getTags( tagList ) {
     var tag,
@@ -25,12 +26,85 @@ define(['jquery', 'uri'],
     return obj;
   }
 
+  setup.template = function() {
+    $( ".ui-code" ).each( function( i, el ) {
+      var html = el.innerHTML;
+      $( el ).text( html );
+    });
+    UI.select( '#select-test', function( val ) {
+      console.log( val );
+    });
+  };
+
+  setup.search = function() {
+    var query = $( ".search-poster" ).attr( "data-query" ),
+        queryKeys = URI.parse( window.location.href ).queryKey,
+        $searchPoster = $( ".search-poster" ),
+        $searchField = $( "#search-field" ),
+        $searchFilter = $( "#search-type" ),
+        $forkBtns = $( ".make-fork-btn" ),
+        $userNameLinks = $( ".user-link" ),
+        $nextBtn = $( ".next-page" ),
+        $prevBtn = $( ".previous-page" );
+
+    function onKeyDown() {
+      $( "html, body" ).animate({ scrollTop: 0 }, 200 );
+      $searchPoster.addClass( "focus" );
+      $searchField.off( "keydown", onKeyDown );
+    }
+
+    $searchFilter.find( "li" ).click( function(){
+      var $this = $( this ),
+          type = $this.attr( "data-value" );
+      $this = $( this );
+      $searchFilter.find( "[name=type]" ).val( type );
+      $searchFilter.find( "[data-selected] > span" ).attr( "class", "icon-" + type );
+      $searchFilter.find( ".ui-on" ).removeClass( "ui-on" );
+      $this.addClass( "ui-on" );
+    });
+
+    if ( query ) {
+      $searchField.val( query );
+      onKeyDown();
+    } else {
+      $searchField.on( "keydown", onKeyDown );
+    }
+
+    $forkBtns.click( function( e ) {
+      e.stopPropagation();
+    });
+
+    $nextBtn.click( function( e ) {
+      queryKeys.page = queryKeys.page ? parseInt( queryKeys.page, 10 ) + 1 : 2;
+      window.location.search = $.param( queryKeys );
+    });
+
+    $prevBtn.click( function( e ) {
+      queryKeys.page = queryKeys.page > 1 ? parseInt( queryKeys.page, 10 ) - 1 : 1;
+      window.location.search = $.param( queryKeys );
+    });
+
+    $userNameLinks.click( function( e ) {
+      queryKeys.page = 1;
+      queryKeys.type = "user";
+      queryKeys.q = this.getAttribute( "data-username" );
+      window.location.search = $.param( queryKeys );
+    });
+  };
+
+  setup.page = function( page ) {
+    if ( setup[ page ] ) {
+      setup[ page ]();
+    }
+  };
+
 
   var self = {
     init: function( options ) {
       makeURL = options.makeURL;
       page = options.page;
       make = Make({ apiURL: makeURL });
+      setup.page( page );
     },
     doSearch: function( options, limit, each ) {
       var sortBy = 'createdAt',

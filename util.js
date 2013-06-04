88df0f706fa781a871035509c2285fbbb07a2e53
Hike Danakian <hike@hdanak.com>
2013-06-04 15:04:47 -0700

    minor cleanup ot in util.js

--- util.js
+++ util.js
@@ -1,5 +1,5 @@
 module.exports = {
-    loadSubmodules: function (path, app) {
+    loadSubmodules: function (path) {
         var exp = {},
             fs = require('fs');
         fs.readdirSync(path).filter(function (f) {
@@ -8,13 +8,13 @@ module.exports = {
                 && (fs.statSync(path+'/'+f).isFile() ? f.match(/\.js$/) : true);
         }).forEach(function (f) {
             try {
-                exp[this.camelCase(f).replace(/\..*/, '')] = require(path+'/'+f);
+                exp[this.camelize(f).replace(/\..*/, '')] = require(path+'/'+f);
             } catch (e) {}
         }, this);
         return exp;
     },
-    camelCase:  function (x) {
-        return x.toLowerCase().replace(/_(\w+)/g, function(m, m1) {
+    camelize:  function (x) {
+        return x.toLowerCase().replace(/_?([^\W_]+)/g, function(m, m1) {
             return m1.charAt(0).toUpperCase() + m1.substr(1);
         }).replace(/\w+/g, function (m, m1) {
             return m.charAt(0).toUpperCase() + m.substr(1);

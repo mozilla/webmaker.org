0f1253fa9754e2297da3ee108c60652e0f2aa96e
Dale Karp <me@dale.io>
2013-06-13 11:27:22 -0400

    [bug 881945] Mentor page

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ app.get( "/learn", routes.page( "learn" ) );
 app.get( "/teach", routes.page( "teach" ) );
 app.get( "/party", routes.page( "party" ) );
 app.get( "/tools", routes.page( "tools" ) );
+app.get( "/mentor", routes.page( "mentor" ) );
 app.get( "/search", routes.search );
 
 app.get( "/details/:id", routes.details );

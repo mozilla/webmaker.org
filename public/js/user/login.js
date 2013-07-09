65681ca9b282f7ebc8f7ab1db93f5dfea038d241
Dale Karp <me@dale.io>
2013-07-09 16:33:46 -0400

    [bug 890872] fix for redirecting back to events when signing in from events

--- public/js/user/login.js
+++ public/js/user/login.js
@@ -2,7 +2,12 @@ define(['jquery', 'sso-ux'],
   function($) {
   "use strict";
   navigator.idSSO.app.onlogin = function(user) {
-    window.location = "/";
+    if( document.referrer.indexOf( 'events' ) !== -1 ){
+      window.location = "/events";
+    }
+    else {
+      window.location = "/";
+    }
   };
   navigator.idSSO.app.onnewuser = function(){
     window.location = "/new";

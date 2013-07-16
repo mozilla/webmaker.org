b647362bf6d16ae3df2b06f877144073031127b0
Hike Danakian <hike@hdanak.com>
2013-07-16 04:50:21 -0700

    Automatically filter pins on load, and misc event-search fixes.

--- controllers/events.js
+++ controllers/events.js
@@ -163,7 +163,7 @@ module.exports = function (init) {
         // pre-process the Date/Time fields
         ['begin', 'end'].forEach(function (pfx) {
             datetime_transform('Date', function (val) {
-                return val ? new Date(val.split(/[-\/]/)) : null;
+                return val ? new Date(val) : null;
             });
             datetime_transform('Time', function (val) {
                 if (!val) return null;
@@ -176,8 +176,7 @@ module.exports = function (init) {
                 transforms[dtf] = function(event) {
                     if (!event[dtf]) return;
                     var new_time = transform(event[dtf]);
-                    if (new_time != "Invalid Date")
-                        return new_time;
+                    return (new_time != "Invalid Date") ? new_time : null;
                 };
             }
         });

6ce5db08daa7d6e6e181dc1e87bf6615bc832e74
Hike Danakian <hike@hdanak.com>
2013-07-22 13:04:35 -0700

    don't require endDate

--- static/js/events/map/forms.js
+++ static/js/events/map/forms.js
@@ -60,7 +60,9 @@ function ($, EventModel, forms) { return function (mapMaker) {
     $.validator.addMethod('afterStartDate', function(val, endElem) {
         var start   = $('#event_beginDate').val(),
             end     = $(endElem).val();
-        return new Date(start) <= new Date(end)
+        if (start && end)
+            return new Date(start) <= new Date(end);
+        else return true;
     }, "End date must be after start date.");
 
     var $createForm = $('form#create-event');

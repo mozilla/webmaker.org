8644be77134b08b67cb7e7006a2141d8ffc8b721
Hike Danakian <hike@hdanak.com>
2013-07-23 00:49:32 -0700

    Improved event-map styling.

--- static/js/events/responsive.js
+++ static/js/events/responsive.js
@@ -5,8 +5,10 @@ function ($) {
         $('main').height(
             $('body').height() - [
                 'header', '#midbar', 'footer'
-            ].map(function(s) { return $(s).height(); })
-             .reduce(function(a, b) { return a + b }, 0));
+            ].map(function(s) {
+                var $s = $(s);
+                return $s.css('display') != 'none' ? $s.height() : 0;
+            }).reduce(function(a, b) { return a + b }, 0));
     }).resize();
 
     setTimeout(scroll.bind(window, 0, 0), 100);

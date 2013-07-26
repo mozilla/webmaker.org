e5f282f20a7c96956413271ead6681080f255944
Christopher De Cairos <christopherd@mozillafoundation.org>
2013-07-26 14:21:52 -0400

    Revert "Bug 890326 - Default search on page load for /search filters on all fields"

--- routes/search.js
+++ routes/search.js
@@ -5,7 +5,6 @@ module.exports = function(req, res) {
   var DEFAULT_TYPE = "tags",
       DEFAULT_QUERY = "webmaker:featured",
       VALID_TYPES = [
-        "all",
         "tags",
         "title",
         "user",
@@ -17,50 +16,35 @@ module.exports = function(req, res) {
       ];
 
   var type = ( req.query.type || DEFAULT_TYPE ).toString(),
+      query = ( req.query.q || DEFAULT_QUERY ).toString(),
       contentType = ( req.query.contentType || '' ).toString(),
       sortByField = ( req.query.sortByField || "createdAt" ).toString(),
       sortByOrder = ( req.query.order || "desc" ).toString(),
       page = ( req.query.page || 1 ).toString(),
-      setToAll = !( req.query.type || req.query.q
-                    || req.query.contentType || req.query.sortByField
-                    || req.query.order || req.query.page ),
-      options = {},
-      query,
-      hideNamespace = false;
-
-  if ( !req.query.q ) {
-    query = DEFAULT_QUERY;
-    hideNamespace = true;
-  } else {
-    query = req.query.q.toString();
-  }
+      options = {};
 
   if ( VALID_TYPES.indexOf( type ) === -1 ) {
     type = DEFAULT_TYPE;
   }
 
-  switch ( type ) {
-    case 'all':
-      make.or();
-      options.title = options.user = options.description = query;
-    case 'tags':
-      var tags = query.split(',');
-      options.tags = [];
-      options.tags[0] = tags.map(function( t ) {
-        // check for hashtag, remove
-        t = t.trim();
-        if ( t[0] === '#' ) {
-          return t.slice(1);
-        }
-        return t;
-      });
-      break;
-    default:
-      // check for '@', remove
-      if ( query[0] === '@' ) {
-        query = query.slice(1);
+  if ( type === 'tags' ) {
+    var tags = query.split(',');
+    options.tags = [];
+    options.tags[0] = tags.map(function( t ) {
+      // check for hashtag, remove
+      t = t.trim();
+      if ( t[0] === '#' ) {
+        return t.slice(1);
       }
-      options[ type ] = query;
+      return t;
+    });
+  }
+  else {
+    // check for '@', remove
+    if ( query[0] === '@' ) {
+      query = query.slice(1);
+    }
+    options[ type ] = query;
   }
 
   if ( contentType ) {
@@ -82,12 +66,8 @@ module.exports = function(req, res) {
     }
     // query can be an array of tags sometimes,
     // so force a string so that it's autoescaped
-    var query = type === "all" ? options.title.toString() : options[type].toString();
+    var query = options[type].toString();
     var showOlder = ( totalHits > page * limit );
-
-    if ( hideNamespace ) {
-      query = "featured";
-    }
 
     res.render( "search.html", {
       hasQuery: !!req.query.q,
@@ -96,7 +76,6 @@ module.exports = function(req, res) {
       pagination: page,
       query: query,
       searchType: type,
-      searchIcon: setToAll ? "all" : type,
       showOlder: showOlder
     });
   });

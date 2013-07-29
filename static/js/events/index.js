f47c5dbf3389064934f2f47de0840df0fc4d16a9
Kate Hudson <k88hudson@gmail.com>
2013-07-29 17:45:31 -0400

    Removed dependency on main repo

--- static/js/events/index.js
+++ static/js/events/index.js
@@ -1,4 +1,4 @@
-require.config({
+    require.config({
     deps: ['main', 'jquery.css3finalize', 'bootstrap-markdown',
             'jquery.timepicker', 'jquery-ui.custom', 'jquery.validate',
             'additional-methods'],
@@ -10,7 +10,7 @@ require.config({
         'text':                 '/ext/js/text',
         'domReady':             '/ext/js/domReady',
 
-        'jquery':               '/ext/js/jquery-1.9.1',
+        'jquery':               '/ext/js/jquery',
         'jquery-ui.custom':     '/ext/js/jquery-ui-1.10.3.custom',
         'jquery.timepicker':    '/ext/js/jquery.timepicker',
         'jquery.validate':      '/ext/js/jquery.validate',

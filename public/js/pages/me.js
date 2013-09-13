2b223165eb79147d52327eeea5f723f06a0852e7
Dale Karp <me@dale.io>
2013-09-13 13:05:50 -0400

    [bug 914160] /me fix only being able to delete once per page refresh

--- public/js/pages/me.js
+++ public/js/pages/me.js
@@ -2,59 +2,59 @@ define(['jquery', 'uri', 'base/ui', 'localized'],
   function ($, URI, UI, localized) {
   'use strict';
 
-  var $body = $("body"),
-      $makes = $(".make"),
-      $deleteBtn = $(".delete-btn"),
-      mainGallery = $(".main-gallery")[0],
-      totalHits,
-      LIMIT,
-      queryKeys = URI.parse( window.location.href ).queryKey,
-      BASE_WIDTH = 240,
-      GUTTER = 20,
-      packery,
-      page;
+  localized.ready(function() {
+    var $body = $("body"),
+        $makes = $(".make"),
+        $deleteBtn = $(".delete-btn"),
+        mainGallery = $(".main-gallery")[0],
+        totalHits,
+        LIMIT,
+        queryKeys = URI.parse( window.location.href ).queryKey,
+        BASE_WIDTH = 240,
+        GUTTER = 20,
+        packery,
+        page;
 
-  // Are we inside thimble or popcorn?
-  var inApp = $body.hasClass("popcorn") || $body.hasClass("thimble");
+    // Are we inside thimble or popcorn?
+    var inApp = $body.hasClass("popcorn") || $body.hasClass("thimble");
 
-  // If the user is not logged in yet,
-  // refresh the page once they are.
-  // This properly generates server side project data
-  // for apps using my makes.
-  if (inApp && !$("meta[name='persona-email']").attr("content")) {
-    navigator.idSSO.app = {
-      onlogin: function(){
-        window.location.replace(window.location);
-      }
-    };
-  }
+    // If the user is not logged in yet,
+    // refresh the page once they are.
+    // This properly generates server side project data
+    // for apps using my makes.
+    if (inApp && !$("meta[name='persona-email']").attr("content")) {
+      navigator.idSSO.app = {
+        onlogin: function(){
+          window.location.replace(window.location);
+        }
+      };
+    }
 
-  // Do we have any makes?
-  if (!$makes.length) {
-    return;
-  }
+    // Do we have any makes?
+    if (!$makes.length) {
+      return;
+    }
 
-  // Set up scrollable container
-  if (inApp) {
-    $(".webmaker-outer-wrapper").css("width", ( ( BASE_WIDTH + GUTTER ) * $makes.length + GUTTER * 2 ) + "px");
-    $("html").css("overflow-y", "hidden");
-  }
+    // Set up scrollable container
+    if (inApp) {
+      $(".webmaker-outer-wrapper").css("width", ( ( BASE_WIDTH + GUTTER ) * $makes.length + GUTTER * 2 ) + "px");
+      $("html").css("overflow-y", "hidden");
+    }
 
-  // Or, set up packery if we are on webmaker.org
-  if (!inApp) {
-    packery = new Packery( mainGallery, {
-      itemSelector: 'div.make',
-      gutter: '.gutter-sizer',
-      transitionDuration: '0.2'
-    });
-  }
+    // Or, set up packery if we are on webmaker.org
+    if (!inApp) {
+      packery = new Packery( mainGallery, {
+        itemSelector: 'div.make',
+        gutter: '.gutter-sizer',
+        transitionDuration: '0.2'
+      });
+    }
 
-  // Set up the delete buttons
-  $deleteBtn.on( "click", function(e) {
-    e.preventDefault();
-    var $this = $(this),
-        makeID = $this.data("make-id");
-    localized.ready(function(){
+    // Set up the delete buttons
+    $deleteBtn.on( "click", function(e) {
+      e.preventDefault();
+      var $this = $(this),
+          makeID = $this.data("make-id");
       if(confirm(localized.get("Are you sure you want to delete this make?"))) {
         $.post("/remove", {
           makeID: makeID,
@@ -75,17 +75,17 @@ define(['jquery', 'uri', 'base/ui', 'localized'],
         });
       }
     });
+
+    page = queryKeys.page ? parseInt(queryKeys.page, 10) : 1;
+
+    if ( mainGallery ) {
+      totalHits = mainGallery.getAttribute("data-total-hits");
+      LIMIT = mainGallery.getAttribute("data-limit");
+
+      UI.pagination( page, totalHits, LIMIT, function( page ) {
+        queryKeys.page = page;
+        window.location.search = $.param( queryKeys );
+      });
+    }
   });
-
-  page = queryKeys.page ? parseInt(queryKeys.page, 10) : 1;
-
-  if ( mainGallery ) {
-    totalHits = mainGallery.getAttribute("data-total-hits");
-    LIMIT = mainGallery.getAttribute("data-limit");
-
-    UI.pagination( page, totalHits, LIMIT, function( page ) {
-      queryKeys.page = page;
-      window.location.search = $.param( queryKeys );
-    });
-  }
 });

f99e77b342e193cf31cab98cbb3e4678e81c49fc
Kate Hudson <k88hudson@gmail.com>
2013-10-21 16:03:48 -0400

    bug 929057 - Descoverability of nav story

--- public/js/pages/teachtheweb.js
+++ public/js/pages/teachtheweb.js
@@ -2,24 +2,42 @@ define(['jquery'],
   function ($) {
 
   var $window = $(window);
+  var $toggleButtonContainer = $('.mentor-story-nav');
   var $toggleButtons = $('.mentor-story-nav a');
+  var totalStories = $('.mentor-story-nav a').length;
+  var $nextButton = $('.go-forward');
+  var $backButton = $('.go-back');
   var $articles = $('.mentor-stories article');
   var $posters = $('.poster-image-container > div');
   var $hiddenScroll = $('.hidden-scroll');
   var gallery = document.querySelector('.make-now-templates');
   var hideScrollTimeout;
 
-  function changeStory(e) {
-    var storyId = 'brooklyn';
-    if (e) {
-      storyId = this.getAttribute('data-id');
-    }
+  function changeStory(storyId) {
     $toggleButtons.removeClass('active');
     $('.mentor-story-nav a[data-id="' + storyId +'"]').addClass('active');
     $('.mentor-stories article.active').removeClass('active');
     $('.poster-image-container > div.active').removeClass('active');
     $('#story-' + storyId).addClass('active');
     $('#poster-' + storyId).addClass('active');
+
+    var currentIndex = $('.mentor-story-nav a').index( $('.mentor-story-nav a.active') );
+    if (currentIndex === 0) {
+      $backButton.hide();
+    } else {
+      $backButton.show();
+    }
+    if (currentIndex >= totalStories - 1) {
+      $nextButton.hide();
+    } else {
+      $nextButton.show();
+    }
+  }
+
+  function getStoryByNumber(n) {
+    // Convert 0 index > selector nth-child, which starts with one
+    n = n + 1;
+    return $toggleButtonContainer.find('a:nth-child('+ n +')').attr('data-id');
   }
 
   function hideScroll() {
@@ -45,6 +63,21 @@ define(['jquery'],
     gutter: '.gutter-make-now'
   });
 
-  $toggleButtons.on('click', changeStory);
+  $toggleButtons.on('click', function() {
+    var storyId = this.getAttribute('data-id');
+    changeStory(storyId);
+  });
+
+  $nextButton.on('click', function() {
+    var currentIndex = $('.mentor-story-nav a').index( $('.mentor-story-nav a.active') );
+    var storyId = getStoryByNumber(currentIndex + 1);
+    changeStory(storyId);
+  });
+
+  $backButton.on('click', function() {
+    var currentIndex = $('.mentor-story-nav a').index( $('.mentor-story-nav a.active') );
+    var storyId = getStoryByNumber(currentIndex - 1);
+    changeStory(storyId);
+  });
 
 });

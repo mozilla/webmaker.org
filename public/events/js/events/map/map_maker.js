ee38ed18d5c222b50c6df9d52cd8eeb006cca25a
Dale Karp <me@dale.io>
2013-11-15 11:38:48 -0500

    [bug 915836] Fix Add an Event failure when address not selected via Autocomplete'

--- public/events/js/events/map/map_maker.js
+++ public/events/js/events/map/map_maker.js
@@ -152,18 +152,54 @@ function ($, google, InfoBubble, OverlappingMarkerSpiderfier, MarkerClusterer) {
           return;
         });
     };
-    MapMaker.prototype.setupAutocomplete = function (input, cityLevel, cb) {
-        var options = { types: cityLevel ? ['(regions)'] : ['geocode', 'establishment'] }; // [] is all
-        var autocomplete = new google.maps.places.Autocomplete(input, options);
-        autocomplete.bindTo('bounds', this.google_map);
 
-        var self = this;
-        // listen for location changes on this text field and center the map on new position
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            var place = autocomplete.getPlace();
-            cb.call(self, place);
-        });
+    // queries the Google Maps Places API for a 'PlacesResult' object,
+    // which contains data like lat/long, formatted address name, etc
+    // See https://developers.google.com/maps/documentation/javascript/reference#Geocoder
+    // for more information
+    function getGooglePlaceResult(addr, autocomplete) {
+      var addrText = $(addr).val();
+      var geocoder = new google.maps.Geocoder();
+
+      geocoder.geocode({address: addrText}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          google.maps.event.trigger(autocomplete, 'place_changed', results[0], true);
+        }
+      });
+    }
+
+    MapMaker.prototype.setupAutocomplete = function (input, cityLevel, cb) {
+      var self = this;
+      var options = { types: cityLevel ? ['(regions)'] : ['geocode', 'establishment'] }; // [] is all
+      var autocomplete = new google.maps.places.Autocomplete(input, options);
+      autocomplete.bindTo('bounds', this.google_map);
+
+      // make sure that we query for address lat/long no matter what
+      $(input).blur(function(e) {
+        getGooglePlaceResult(input, autocomplete);
+        return false;
+      })
+      .keydown(function(e) {
+        if (e.which === 13) {
+          getGooglePlaceResult(input, autocomplete);
+          return false;
+        }
+      });
+
+      // listen for location changes on this text field and center the map on new position
+      google.maps.event.addListener(autocomplete, 'place_changed', function(results, needCoords) {
+        var place;
+
+        if (needCoords) {
+          place = results;
+        } else {
+          place = autocomplete.getPlace();
+        }
+
+        cb.call(self, place);
+      });
     };
+
     MapMaker.prototype.updateLocation = function() {
         var google_map = this.google_map;
         function getCurrentPosition_success(pos) {

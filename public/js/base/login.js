a7a232b8a7e7fe03980b2ab833a9cf96379221be
Ali Al Dallal <ali@alicoding.com>
2014-03-11 20:52:30 -0400

    bug982436 - Fix url when click on username

--- public/js/base/login.js
+++ public/js/base/login.js
@@ -11,12 +11,11 @@ define(['jquery', 'webmaker-auth-client/webmaker-auth-client'], function ($, Web
   function toggleUserData(userData) {
     var placeHolder = $('#identity');
     var userElement = $('div.user-name');
-    var lang = $('html').attr('lang') || 'en-US';
 
     if (userData) {
       placeHolder.html('<img src="https://secure.gravatar.com/avatar/' +
         userData.emailHash + '?s=26&d=https%3A%2F%2Fstuff.webmaker.org%2Favatars%2Fwebmaker-avatar-44x44.png" alt="">' +
-        '<a href="/' + lang + '/account">' + userData.username + "</a>");
+        '<a href="' + $("#loginURL").attr('href') + '">' + userData.username + "</a>");
     } else {
       userElement.html('<span id="identity"></span>');
     }

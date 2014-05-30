4f36d09ffe07dc0f510b8bb68535b22ced7e8cef
Aki Rose <hi@akibraun.com>
2014-05-30 11:25:15 -0500

    Fixes Bug 1018281 - Remove $anchorScroll from resources MVP

--- public/js/angular/directives.js
+++ public/js/angular/directives.js
@@ -51,54 +51,6 @@ angular
       }
     };
   })
-  .directive('scrollTo', ['$anchorScroll', '$location',
-    function ($anchorScroll, $location) {
-      return {
-        restrict: 'A',
-        link: function (scope, el, attrs) {
-          var id = attrs.href.replace('#', '');
-          if (!id) {
-            return;
-          }
-          el.on('click', function (e) {
-            e.preventDefault();
-            $location.hash(id);
-            $anchorScroll();
-          });
-        }
-      };
-    }
-  ])
-  .directive('scrollSidebar', ['$window',
-    function ($window) {
-      return {
-        restrict: 'EA',
-        priority: 0,
-        link: function (scope, el, attrs) {
-          var windowEl = $($window);
-          var offset = +attrs.offset;
-          var elTop = el.offset().top + offset;
-
-          windowEl.scroll(function () {
-            var scrollTop = windowEl.scrollTop();
-            if (scrollTop >= elTop) {
-              el.css({
-                position: 'fixed',
-                top: offset + 'px'
-              });
-
-            } else {
-              el.css({
-                position: '',
-                top: ''
-              });
-            }
-          });
-
-        }
-      };
-    }
-  ])
   .directive('retinaImage', ['$window',
     function ($window) {
       'use strict';

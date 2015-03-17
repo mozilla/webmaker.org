7b094b2dc42835c2012e9ccb2d0bc31da640edad
Scott <scott@localhost.localdomain>
2015-03-16 23:50:29 -0400

    toggle nav manu while not in angular

--- public/js/base/navigation.js
+++ public/js/base/navigation.js
@@ -5,20 +5,18 @@ define(['jquery', 'analytics'], function ($, analytics) {
     var currentSection = $mainNavContainer.data('current-section');
     var lang = $('html').attr('lang') || 'en-US';
 
-    var $expandedNavTriggers = $('.navigation-more');
-    var $expandedNav = $('#expanded-navigation');
-    var $activeNavItems = $('[data-page="' + currentPage + '"], [data-section="' + currentSection + '"]');
-
+    var $expandedNavTriggers = $('.navbar-toggle');
+    var $navPrimary = $('#navigation-primary');
+    var $navSecondary = $('#navigation-secondary');
     var $searchTrigger = $('#nav-search-trigger');
     var $searchInput = $('#nav-search-input');
 
     function toggleExpandedMenu(e) {
       e.preventDefault();
-      $expandedNav.toggleClass('on');
-      $activeNavItems.toggleClass('active');
-      $expandedNavTriggers.toggleClass('active');
+      $navSecondary.toggleClass('collapse');
+      $navPrimary.toggleClass('collapse');
       analytics.event('Expand Menu', {
-        label: $expandedNav.hasClass('on') ? 'Expand' : 'Collapse'
+        label: $navPrimary.hasClass('collapse') ? 'Collapse' : 'Expand'
       });
     }
 
@@ -39,26 +37,6 @@ define(['jquery', 'analytics'], function ($, analytics) {
       analytics.event('Menu Navigation Clicked', {
         label: name
       });
-    });
-
-    // Nav Accordion (For mobile)
-
-    var $linkGroupHeaders = $expandedNav.find('.navigation-list li:first-child');
-    var linkGroups = [];
-    var $activeGroup;
-
-    $expandedNav.find('.navigation-list').each(function () {
-      $(this).addClass('collapsed');
-      linkGroups.push($(this).find('li:not(:first-child)'));
-    });
-
-    $linkGroupHeaders.on('click', function () {
-      if ($activeGroup) {
-        $activeGroup.addClass('collapsed');
-      }
-
-      $(this).parent().toggleClass('collapsed');
-      $activeGroup = $(this).parent();
     });
 
     $expandedNavTriggers.on('click', toggleExpandedMenu);

8143b5b84936e373af7583f3570039bb43fe17c8
Jon Buckley <jon@jbuckley.ca>
2015-05-26 11:59:30 -0400

    Fix #1412 - Add redirect for /literacy and /resources

--- routes/redirect.js
+++ routes/redirect.js
@@ -103,7 +103,9 @@ module.exports = function (app) {
   }, {
     route: 'https://teach.mozilla.org/teach-like-mozilla/web-literacy',
     paths: [
+      '/resources',
       '/resources/*',
+      '/literacy',
       '/literacy/*'
     ]
   }, {
